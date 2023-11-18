3f0deebbd39fcb51d3dc4fd4cd0f019b7d735cae
Anna <annasyry@yandex.ru>
2023-11-18 23:26:54 +0300

    Bug fixes, PEP8

--- game.py
+++ game.py
@@ -23,6 +23,7 @@ def random_predict(number: int = 1) -> int:
             break  # выход из цикла если угадали
     return count
 
+
 def number_predict(number: int = 1) -> int:
     """Угадываем число последовательным разбиением интервала на 2
 
@@ -31,41 +32,41 @@ def number_predict(number: int = 1) -> int:
 
     Returns:
         int: Число попыток
-    """ 
-    
+    """
+
     count = 0
     predict_number = 0
     min_num = 1
-    max_num = 101 
+    max_num = 101
 
     while True:
         count += 1
         predict_number = min_num + ((max_num - min_num) // 2)
         if number == predict_number:
             break  # выход из цикла если угадали
-        elif number > predict_number:
-            min_num = predict_number
+        if number > predict_number:
+            min_num = predict_number + 1
         else:
-            max_num = predict_number
+            max_num = predict_number - 1
     return count
 
 
-
-def score_game(random_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+def score_game(func) -> int:
+    """Какое количество попыток нужно алгоритму для угадывания числа в среднем за 1000 попыток
 
     Args:
-        random_predict ([type]): функция угадывания
+        number_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    # np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(
+        1, 101, size=1000)  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(number_predict(number))
+        count_ls.append(func(number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -74,4 +75,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(number_predict)
