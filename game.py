cbb6899ab418f386f687b0fd90126615d6b7034f
Anna <annasyry@yandex.ru>
2023-11-18 23:47:03 +0300

    Removed function random_guess as unnecessary

--- game.py
+++ game.py
@@ -5,25 +5,6 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
-    count = 0
-
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
-    return count
-
-
 def number_predict(number: int = 1) -> int:
     """Угадываем число последовательным разбиением интервала на 2
 
